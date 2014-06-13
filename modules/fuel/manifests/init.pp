72d3aae9d82f084540a71828c4fae165d4376e4e
matts <matts@lampscaled.net>
2014-06-13 10:22:43 -0700

    first commit

---
+++ modules/fuel/manifests/init.pp
@@ -0,0 +1,42 @@
+class fuel() {
+
+package { 'unzip': ensure => present }
+
+file { "/fuel/vbox-scripts-5.0.zip":
+        owner   => "root",
+        group   => "root",
+        mode    => 0644,
+        source  => "puppet:///modules/fuel/vbox-scripts-5.0.zip",
+}
+
+exec { 'unpack':
+        path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ],
+        command => "unzip -u /fuel/vbox-scripts-5.0.zip -d /fuel",
+        require => File['/fuel/vbox-scripts-5.0.zip'],
+}
+
+file { "/fuel/virtualbox/config.sh":
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/fuel/config.sh",
+        require => Exec['unpack'],
+}
+
+file { "/fuel/MirantisOpenStack-5.0.iso":
+        owner   => "root",
+        group   => "root",
+        mode    => 0644,
+        source  => "puppet:///modules/fuel/MirantisOpenStack-5.0.iso",
+}
+
+file { "/fuel/virtualbox/iso/MirantisOpenStack-5.0.iso":
+        owner   => "root",
+        group   => "root",
+        mode    => 0644,
+        ensure  => link,
+        target  => '/fuel/MirantisOpenStack-5.0.iso',
+        require => Exec['unpack'],
+}
+
+}
---
+++ production/oobuntoo.pp
@@ -0,0 +1,3 @@
+node 'oobuntoo' {
+  include fuel
+}
